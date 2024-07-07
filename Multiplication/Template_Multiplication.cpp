ac1ac22717af13ef0211f5e739dec6eb66c9053f
baris <baris@BARIS_SOMEROGLU>
2024-07-07 19:09:53 +0300

    Add project files.

---
+++ Multiplication/Template_Multiplication.cpp
@@ -0,0 +1,33 @@
+﻿/*
+In this code we multiply two value and return the result as type of first value.
+Purpose of the code is learn how to use template.
+
+Developer: Barış Someroğlu
+Date: 07.07.2024 - 07:10 pm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// Template function definiton
+template <typename T, typename U>
+T Multiplication(T x, U y)
+{
+	return x * y;
+}
+
+int main()
+{
+	// Change colour of console
+	system("color E9");
+
+	// Send different value to the template function
+	cout << "Multiplication1 : " << Multiplication(10, 7.98) << endl;
+	cout << "\nMultiplication2 : " << Multiplication(7.98, 10) << endl;
+	cout << "\nMultiplication3 : " << Multiplication('0', 10) << endl;
+	cout << "\nMultiplication4 : " << Multiplication(7.98, '10') << endl;
+	cout << "\nMultiplication2 : " << Multiplication('7.98', '11') << endl;
+
+	return 0;
+}
